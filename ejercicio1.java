9eee783b1dabe24f85aa59eb63968e71f815bb9f
Alexander Dilas <alexanderdilas13@gmail.com>
2025-06-21 13:18:37 -0500

    se sube el archivo ejercicio1

---
+++ ejercicio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ejercicio1{
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Ingrese su edad: ");
+        int edad = sc.nextInt();
+
+        if(edad < 0){
+            System.out.println("Ingrese una edad válida.");
+        } else{
+            if(edad < 18){
+                System.out.println("Es menor de edad.");
+            } else{
+                System.out.println("Es mayor de edad.");
+            }
+        }
+        
+    }
+}
